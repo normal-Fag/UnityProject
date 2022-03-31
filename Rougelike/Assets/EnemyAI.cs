5ab4292d584492edf8f190a17f2ef0d1cb7858e6
normal-Fag <72517164+normal-Fag@users.noreply.github.com>
2022-03-31 22:35:59 +0700

    Enemy AI / A*

---
+++ Rougelike/Assets/EnemyAI.cs
@@ -0,0 +1,113 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class EnemyAI : MonoBehaviour
+{
+    [Header("Pathfinding")]
+    [SerializeField] public Transform target;
+    [SerializeField] public float activateDistance = 1f;
+    [SerializeField] public float pathUpdateSeconds = 0.5f;
+
+    [Header("Phisics")]
+    [SerializeField] public float speed = 400f;
+    [SerializeField] public float nextWaypointDistance = 3f;
+    [SerializeField] public float jumpNodeHeightRequirement = 0.8f;
+    [SerializeField] public float jumpModifier = 0.3f;
+    [SerializeField] public float jumpCheckOffset = 0.1f;
+
+    [Header("Custom Behavior")]
+    [SerializeField] public bool followingEnabled = true;
+    [SerializeField] public bool jumpEnebled = true;
+    [SerializeField] public bool directionLookEnabled = true;
+
+    private Path path;
+    private int currentWayPoint = 0;
+    private bool isGrounded = false;
+    private Seeker seeker;
+    private Rigidbody2D rb;
+
+    public void Start()
+    {
+        seeker = GetComponent<Seeker>();
+        rb = GetComponent<Rigidbody2D>();
+
+        InvokeRepeating("UpdatePath", 0f, pathUpdateSeconds);
+    }
+
+    void FixedUpdate()
+    {
+        if (TargetIsInDistance() && followingEnabled)
+        {
+            PathFollow();
+        }
+    }
+
+    private void UpdatePath()
+    {
+        if (followingEnabled && TargetIsInDistance() && seeker.IsDone())
+        {
+            seeker.StartPath(rb.position, target.position, OnPathComplete);
+        }
+    }
+
+    private void PathFollow()
+    {
+        if (path == null)
+            return;
+
+        if (currentWayPoint >= path.vectorPath.Count)
+            return;
+
+        // Возвращает true если обьект соприкосается с чем-либо
+        isGrounded = Physics2D.Raycast(transform.position, -Vector3.up, GetComponent<Collider2D>().bounds.extents.y + jumpCheckOffset);
+
+        // Вычисление направления движения
+        Vector2 direction = ((Vector2)path.vectorPath[currentWayPoint] - rb.position).normalized;
+        Vector2 force = direction * speed * Time.deltaTime;
+
+        // Прыжок
+        if (jumpEnebled && isGrounded)
+        {
+            if (direction.y > jumpNodeHeightRequirement)
+                rb.AddForce(Vector2.up * speed * jumpModifier);
+        }
+
+        // Движение
+        rb.AddForce(force);
+
+        // Nex Waypoint
+        float distance = Vector2.Distance(rb.position, path.vectorPath[currentWayPoint]);
+        if(distance < nextWaypointDistance)
+        {
+            currentWayPoint++;
+        }
+
+        if (directionLookEnabled)
+        {
+            if (rb.velocity.x > 0.05f)
+            {
+                transform.localScale = new Vector3(-1f * Mathf.Abs(transform.localScale.x), transform.lossyScale.y, transform.localScale.z);
+            }
+            else if (rb.velocity.x < 0.05f)
+            {
+                transform.localScale = new Vector3(Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
+            }
+        }
+    }
+
+    private bool TargetIsInDistance()
+    {
+        return Vector2.Distance(transform.position, target.transform.position) < activateDistance;
+    }
+
+    private void OnPathComplete(Path p)
+    {
+        if (!p.error)
+        {
+            path = p;
+            currentWayPoint = 0;
+        }
+    }
+}
---
+++ Rougelike/Assets/Scripts/playerMovement.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+    [Header("General")]
+    [SerializeField] public int playerHP = 100;
+
+    [Header("Movement")]
+    [SerializeField] public int playerSpeed = 10;
+    [SerializeField] public float playerJumpForce = 20;
+
+    private Rigidbody2D playerRb;
+    private Animator playerAnimator;
+    private Sensor_Prototype playerGroundSensor;
+
+    private bool isGrounded;
+    private bool isMoving;
+    private int facingDiections = 1;
+
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody2D>();
+        playerAnimator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        float input = Input.GetAxis("Horizontal");
+
+        float slowDownSpeed = isMoving ? 1.0f : 0.5f;
+
+        playerRb.velocity = new Vector2(input * playerSpeed * slowDownSpeed, playerRb.velocity.y);
+
+        if (Input.GetButton("Jump"))
+        {
+            playerRb.velocity = new Vector2(playerRb.velocity.x, playerJumpForce);
+        }
+
+        if (input > 0)
+        {
+            GetComponent<SpriteRenderer>().flipX = false;
+            facingDiections = 1;
+        }
+        else if (input < 0)
+        {
+            GetComponent<SpriteRenderer>().flipX = true;
+            facingDiections = -1;
+        }
+    }
+    public void takeDamage()
+    {
+        playerHP--;
+        if (playerHP < 1)
+            playerDeath();
+    }
+    private void playerDeath()
+    {
+        playerAnimator.SetBool("noBlood", false);
+    }
+}
