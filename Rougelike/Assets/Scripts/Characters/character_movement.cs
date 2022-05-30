17648a810aa20471afcf0f44e907f242c7add75a
Shtrohaim <79344587+Shtrohaim@users.noreply.github.com>
2022-05-30 14:42:59 +0700

    Resolved merge conflict by incorporating both suggestions.

---
+++ Rougelike/Assets/Scripts/Characters/character_movement.cs
@@ -0,0 +1,458 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class character_movement : MonoBehaviour
+{
+
+    [SerializeField] private float m_maxSpeed = 4.5f;
+    [SerializeField] private float m_jumpForce = 7.5f;
+    [SerializeField] private UI_Inventory uiInventory;
+
+    private Animator m_animator;
+    public Rigidbody2D m_body2d;
+    private Sensor_Prototype m_groundSensor;
+    private AudioSource m_audioSource;
+    private AudioEffects m_audioManager;
+
+    public bool m_grounded = false;
+    private bool m_moving = false;
+    public static float m_facingDirection = 1;
+
+    private float m_disableMovementTimer = 0.0f;
+
+    public bool stopingAction;
+
+    private int stopMoving;
+    private bool isChargeAttack;
+
+    private Inventory inventory;
+
+
+    public int max_hp = 100;
+    public static float max_hp_for_ui;
+    public static int currentHp;
+
+
+    private bool hasHPBuff = false;
+    private bool HealthRegCD = false;
+    private int HealthPotionCD;
+    public static float currentHealthPotionCD;
+    private bool hasHealthPotionCD;
+    private int HPBuffCD;
+    public static float currentHPBuffCD;
+    private bool hasHPBuffCD;
+
+
+    float currentDashTimmer;
+    bool isRolling = false;
+    public float rollDistance;
+    public float startDashTimer;
+    public float trapDistanceEvade;
+
+    bool isTraping = false;
+
+
+
+    // Use this for initialization
+    private void Start()
+    {
+        m_animator = GetComponent<Animator>();
+        m_body2d = GetComponent<Rigidbody2D>();
+        m_audioSource = GetComponent<AudioSource>();
+        m_audioManager = AudioEffects.instance;
+        m_groundSensor = transform.Find("GroundSensor").GetComponent<Sensor_Prototype>();
+        stopingAction = false;
+
+        currentHp = max_hp;
+        max_hp_for_ui = max_hp;
+
+        inventory = new Inventory(UseItem);
+        uiInventory.SetInventory(inventory);
+        uiInventory.SetCharacter(this);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        max_hp_for_ui = max_hp;
+        // Decrease timer that disables input movement. Used when attacking
+        m_disableMovementTimer -= Time.deltaTime;
+
+        //Check if character just landed on the ground
+        if (!m_grounded && m_groundSensor.State())
+        {
+            m_grounded = true;
+            m_animator.SetBool("Grounded", m_grounded);
+        }
+
+        //Check if character just started falling
+        if (m_grounded && !m_groundSensor.State())
+        {
+            m_grounded = false;
+            m_animator.SetBool("Grounded", m_grounded);
+        }
+
+        // -- Handle input and movement --
+        float inputX = 0.0f;
+
+        if (m_disableMovementTimer < 0.0f)
+            inputX = CrossPlatformInputManager.GetAxis("Horizontal");
+
+        // GetAxisRaw returns either -1, 0 or 1
+        float inputRaw = CrossPlatformInputManager.GetAxisRaw("Horizontal");
+        // Check if current move input is larger than 0 and the move direction is equal to the characters facing direction
+        if (Mathf.Abs(inputRaw) > Mathf.Epsilon && Mathf.Sign(inputRaw) == m_facingDirection)
+            m_moving = true;
+
+        else
+            m_moving = false;
+
+        // Swap direction of sprite depending on move direction
+        if (inputRaw > 0 && !isChargeAttack)
+        {
+            m_facingDirection = 1;
+            transform.localScale = new Vector3(2.0f, 2.0f, 1.0f);
+        }
+
+        else if (inputRaw < 0 && !isChargeAttack)
+        {
+
+            m_facingDirection = -1;
+            transform.localScale = new Vector3(-2.0f, 2.0f, 1.0f);
+
+        }
+
+        // SlowDownSpeed helps decelerate the characters when stopping
+
+        // Set movement
+        m_body2d.velocity = new Vector2(inputX * m_maxSpeed * stopMoving, m_body2d.velocity.y);
+
+        // Set AirSpeed in animator
+        m_animator.SetFloat("AirSpeedY", m_body2d.velocity.y);
+
+
+        // -- Handle Animations --
+        //Jump
+        if (CrossPlatformInputManager.GetButtonDown("Jump") && m_grounded && m_disableMovementTimer < 0.0f)
+        {
+            Jump();
+        }
+        //Run
+        else if (m_moving)
+            m_animator.SetInteger("AnimState", 1);
+
+        //Idle
+        else
+            m_animator.SetInteger("AnimState", 0);
+
+
+        if(currentHp > max_hp)
+        {
+            currentHp = max_hp;
+        }
+
+
+
+         if (stopingAction)
+        {
+            stopMoving = 0;
+            isChargeAttack = true;
+        }
+        else
+        {
+            stopMoving = 1;
+            isChargeAttack = false;
+        }
+
+        void Jump()
+        {
+            m_animator.SetTrigger("Jump");
+            m_grounded = false;
+            m_animator.SetBool("Grounded", m_grounded);
+            m_body2d.velocity = new Vector2(m_body2d.velocity.x, m_jumpForce);
+            m_groundSensor.Disable(0.2f);
+        }
+
+
+
+        void AE_runStop()
+        {
+            m_audioManager.PlaySound("RunStop");
+        }
+
+        void AE_footstep()
+        {
+            m_audioManager.PlaySound("Footstep");
+        }
+
+        void AE_Jump()
+        {
+            m_audioManager.PlaySound("Jump");
+        }
+
+        void AE_Landing()
+        {
+            m_audioManager.PlaySound("Landing");
+
+        }
+
+        if (hasHPBuffCD)
+        {
+
+            currentHPBuffCD -= 1f / HPBuffCD * Time.deltaTime;
+            CheckCDinInventory(new Item { itemType = Item.ItemType.HPBuff, amount = 1, CD = 10 });
+            if (currentHPBuffCD <= 0)
+            {
+                hasHPBuffCD = false;
+                foreach (Item item in inventory.GetItemList())
+                {
+                    if (item.itemType == Item.ItemType.HPBuff)
+                    {
+                        item.isCD = false;
+                    }
+                }
+            }
+        }
+
+        if (hasHealthPotionCD)
+        {
+            currentHealthPotionCD -= 1f / HealthPotionCD * Time.deltaTime;
+            CheckCDinInventory(new Item { itemType = Item.ItemType.HealthPotion, amount = 1, CD = 30 });
+            if (currentHealthPotionCD <= 0)
+            {
+                hasHealthPotionCD = false;
+                foreach (Item item in inventory.GetItemList())
+                {
+                    if (item.itemType == Item.ItemType.HealthPotion)
+                    {
+                        item.isCD = false;
+                    }
+                }
+            }
+        }
+
+
+
+        if (isRolling)
+        {
+            m_animator.SetInteger("RollingState", 1);
+            m_body2d.velocity = transform.right * rollDistance * character_movement.m_facingDirection;
+            Physics2D.IgnoreLayerCollision(3, 7, true);
+            currentDashTimmer -= Time.deltaTime;
+
+            if (currentDashTimmer <= 0)
+            {
+                isRolling = false;
+                m_animator.SetInteger("RollingState", 0);
+                Physics2D.IgnoreLayerCollision(3, 7, false);
+            }
+        }
+
+        if (isTraping)
+        {
+            Physics2D.IgnoreLayerCollision(3, 7, true);
+            m_body2d.velocity = transform.right * trapDistanceEvade * character_movement.m_facingDirection * -1f;
+            currentDashTimmer -= Time.deltaTime;
+
+
+            if (currentDashTimmer <= 0)
+            {
+                Physics2D.IgnoreLayerCollision(3, 7, false);
+                isTraping = false;
+            }
+        }
+
+    }
+
+    public Vector2 GetPosition()
+    {
+        return transform.position;
+    }
+    public float GetFacing()
+    {
+        return m_facingDirection;
+    }
+
+    private void UseItem (Item item, int index)
+    {
+        switch (item.itemType)
+        {
+            default:
+            case Item.ItemType.HealthPotion:
+                HealthPotionCD = item.CD;
+                hasHealthPotionCD = true;
+                CheckCDinInventory(item);
+                inventory.RemoveItem(item, index);
+                currentHealthPotionCD = 1f;
+                StartCoroutine(useHealthPotion(item.CD));
+
+                break;
+            case Item.ItemType.HPBuff:
+                HPBuffCD = item.CD;
+                hasHPBuffCD = true;
+                CheckCDinInventory(item);
+                inventory.RemoveItem(item, index);
+                currentHPBuffCD = 1f;
+                StartCoroutine(useHealthBuff(item.CD));
+
+                break;
+
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        ItemWorld itemWorld = collision.GetComponent<ItemWorld>();
+        if(collision.GetComponent<ItemWorld>() != null)
+        {
+            List<Item> items = inventory.GetItemList();
+            if(items.Count < 6)
+            {
+                if (itemWorld.GetItem().itemType == Item.ItemType.HealthPotion)
+                {
+                    itemWorld.GetItem().isCD = hasHealthPotionCD;
+                }
+                if (itemWorld.GetItem().itemType == Item.ItemType.HPBuff)
+                {
+                    itemWorld.GetItem().isCD = hasHPBuffCD;
+                }
+                inventory.AddItem(itemWorld.GetItem());
+                itemWorld.DestroySelf();
+            }
+            else
+            {
+                foreach (Item item in items)
+                {
+                    if (itemWorld.GetItem().itemType == item.itemType && item.amount < inventory.max_stack)
+                    {
+                        inventory.AddItem(itemWorld.GetItem());
+                        itemWorld.DestroySelf();
+                        break;
+                    }
+                   
+                }
+            }
+           
+            
+        }
+
+    }
+
+
+    public void Take_Damage(int damage, int enemy_facingDirection)
+    {
+        if ((m_animator.GetCurrentAnimatorStateInfo(0).IsName("defend")
+        || m_animator.GetCurrentAnimatorStateInfo(0).IsName("ultimate"))
+        && m_facingDirection != enemy_facingDirection
+        && gameObject.GetComponent<fire_warrior_controler>() != null)
+        {
+            damage = damage / 5;
+            currentHp -= damage;
+            fire_warrior_controler.number_of_rage += 10;
+            Debug.Log("Blocked");
+        }
+        else if (gameObject.GetComponent<fire_warrior_controler>() != null)
+        {
+            fire_warrior_controler.number_of_rage += 3;
+            currentHp -= damage;
+            Debug.Log("Not Blocked");
+        }
+       
+        if((!isRolling || !isTraping) && gameObject.GetComponent<rouge_controller>() != null)
+        {
+            currentHp -= damage;
+        }
+
+        if (m_animator.GetCurrentAnimatorStateInfo(0).IsName("defend")
+       && m_facingDirection != enemy_facingDirection
+       && gameObject.GetComponent<character_water_priest_controller>() != null)
+        {
+            character_water_priest_controller.number_of_mana -= (character_water_priest_controller.max_mana_for_ui * 25) / 100;
+            character_water_priest_controller.manaCharge += 0.34f;
+            Debug.Log("Blocked");
+        }
+        else if (gameObject.GetComponent<character_water_priest_controller>() != null)
+        {
+             currentHp -= damage;
+            Debug.Log("Not Blocked");
+        }
+
+
+        if(m_animator.GetCurrentAnimatorStateInfo(0).IsName("defend") 
+            || m_animator.GetCurrentAnimatorStateInfo(0).IsName("ultimate") 
+            || m_animator.GetCurrentAnimatorStateInfo(0).IsName("heal")
+            || m_animator.GetCurrentAnimatorStateInfo(0).IsName("roll") 
+            || m_animator.GetCurrentAnimatorStateInfo(0).IsName("trap_cast")
+            || m_animator.GetCurrentAnimatorStateInfo(0).IsName("tumble")
+            || m_animator.GetCurrentAnimatorStateInfo(0).IsName("sp_atk")
+            || m_animator.GetCurrentAnimatorStateInfo(0).IsName("sp_attack")) { }
+        else
+        {
+            m_animator.SetTrigger("Hurt");
+        }
+
+
+        if (currentHp <= 0)
+        {
+            m_animator.SetTrigger("Death");
+            GetComponent<BoxCollider2D>().enabled = false;
+            GetComponent<Rigidbody2D>().isKinematic = true;
+
+        }
+    }
+
+    private void CheckCDinInventory(Item item)
+    {
+        foreach (Item itemCD in inventory.GetItemList())
+        {
+            if (item.itemType == itemCD.itemType)
+            {
+                itemCD.isCD = true;
+            }
+
+        }
+    }
+    public IEnumerator useHealthPotion(int seconds)
+    {
+
+        currentHp += (max_hp * 40) / 100;
+        HealthRegCD = true;
+
+        yield return new WaitForSeconds(seconds);
+        HealthRegCD = false;
+    }
+
+    public IEnumerator useHealthBuff(int seconds)
+    {
+        int procHP = (currentHp * 100) / max_hp;
+        max_hp += 50;
+        currentHp = (max_hp * procHP) / 100;
+        hasHPBuff = true;
+
+        yield return new WaitForSeconds(seconds);
+
+        procHP = (currentHp * 100) / max_hp;
+        max_hp -= 50;
+        currentHp = (max_hp * procHP) / 100;
+        hasHPBuff = false;
+    }
+
+    public void Roll()
+    {
+        m_animator.SetTrigger("Roll");
+        isRolling = true;
+        currentDashTimmer = startDashTimer;
+        m_body2d.velocity = Vector2.zero;
+    }
+
+    public void TrapCast()
+    {
+        isTraping = true;
+        m_animator.SetTrigger("trap_skill");
+        m_body2d.velocity = Vector2.zero;
+        currentDashTimmer = startDashTimer;
+
+    }
+}
---
+++ Rougelike/Assets/Scripts/Characters/rouge_controller.cs
@@ -0,0 +1,279 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rouge_controller : MonoBehaviour
+{
+    [Header("Variables")]
+    [SerializeField] float timer = 0;
+    [SerializeField] int attackDamage = 20;
+    private Animator m_animator;
+    private AudioSource m_audioSource;
+    private AudioEffects m_audioManager;
+
+    private Rigidbody2D m_body2d;
+
+
+    private bool isAttack1 = false;
+    private bool isChargeAttack = false;
+
+    public Transform throwPoint;
+    public GameObject dagger_throw;
+    public GameObject skill_dagger;
+
+    private GameObject weapon;
+    private wp_hitbox weapon_hb;
+    private character_movement character_Movement;
+
+    bool isDelayAction = false;
+    public float actionDelay = 0.5f;
+
+
+    public float attackRate = 2f;
+
+    public static int number_of_dagger;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        weapon = GameObject.Find("hit_dagger");
+        character_Movement = GetComponent<character_movement>();
+        m_body2d = GetComponent<Rigidbody2D>();
+        m_animator = GetComponent<Animator>();
+        weapon_hb = weapon.GetComponent<wp_hitbox>();
+        weapon_hb.canAttack = false;
+        m_audioSource = GetComponent<AudioSource>();
+        m_audioManager = AudioEffects.instance;
+        number_of_dagger = 15;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKey("j"))
+        {
+            timer += Time.deltaTime;
+        }
+        if (Input.GetKeyDown("j"))
+        {
+            timer = 0;
+        }
+
+
+        if (Input.GetKeyDown("j") && !isAttack1 && !isDelayAction)
+        {
+            StartCoroutine(ActionDelay(actionDelay + actionDelay * 0.3f, "Attack1"));
+
+        }
+
+
+        else if (Input.GetKeyDown("j") && isAttack1)
+        {
+            Attack2();
+        }
+
+        //Roll
+
+        if (Input.GetKeyDown("k") && character_Movement.m_grounded && !isDelayAction)
+        {
+            StartCoroutine(ActionDelay(actionDelay + actionDelay * 0.2f, "Roll"));
+
+        }
+
+        else if (Input.GetKeyDown("u") && character_Movement.m_grounded && !isDelayAction && number_of_dagger > 0)
+        {
+            StartCoroutine(ActionDelay(actionDelay * 2, "TrapCast"));
+
+        }
+
+        else if (Input.GetKeyUp("i") && !isDelayAction && number_of_dagger > 0)
+        {
+            StartCoroutine(ActionDelay(actionDelay, "Throw_Dagger"));
+
+        }
+
+        else if (Input.GetKeyDown("l") && character_Movement.m_grounded && !isDelayAction)
+        {
+            StartCoroutine(ActionDelay(actionDelay, "SpecialAttack"));
+
+        }
+
+        if (timer > 0.4 && character_Movement.m_grounded)
+        {
+            if ((Input.GetKeyUp("j")) && !isDelayAction)
+            {
+                StartCoroutine(ActionDelay(actionDelay * 3, "ChargeAttack"));
+
+            }
+        }
+
+      
+
+        else if (m_animator.GetCurrentAnimatorStateInfo(0).IsName("3_atk") 
+            || m_animator.GetCurrentAnimatorStateInfo(0).IsName("sp_atk") 
+            || m_animator.GetCurrentAnimatorStateInfo(0).IsName("trap_cast"))
+        {
+            character_Movement.stopingAction = true;
+        }
+        else
+        {
+            character_Movement.stopingAction = false;
+        }
+
+  
+
+        if (m_animator.GetCurrentAnimatorStateInfo(0).IsName("3_atk")
+         || m_animator.GetCurrentAnimatorStateInfo(0).IsName("2_atk")
+         || m_animator.GetCurrentAnimatorStateInfo(0).IsName("1_atk")
+         || m_animator.GetCurrentAnimatorStateInfo(0).IsName("sp_atk"))
+        {
+            weapon_hb.canAttack = true;
+        }
+        else
+        {
+            weapon_hb.canAttack = false;
+        }
+    }
+
+    void Attack1()
+    {
+        isAttack1 = true;
+        m_animator.SetTrigger("Attack");
+        timer = 0;
+    }
+
+    void Attack2()
+    {
+        isAttack1 = false;
+        m_animator.SetTrigger("Attack_2");
+        timer = 0;
+    }
+
+  
+
+    void ThrowDagger()
+    {
+        m_animator.SetTrigger("Throw_dagger");
+        throwPoint.rotation = Quaternion.Euler(0, 0, 0);
+        StartCoroutine(Shoot());
+
+    }
+
+    void SpecialAttack()
+    {
+        m_animator.SetTrigger("sp_atk");
+      
+    }
+
+
+    void ChargeAttack()
+    {
+        float first_atk = 0;
+        isAttack1 = false;
+        m_animator.SetTrigger("Attack_3");
+        weapon_hb.hasRepulsion = true;
+        weapon_hb.repulsion = 3;
+        timer = 0;
+
+
+    }
+    IEnumerator ActionDelay(float time, string action)
+    {
+        isDelayAction = true;
+        switch (action)
+        {
+            case "Roll":
+               character_Movement.Roll();
+                break;
+            case "TrapCast":
+                character_Movement.TrapCast();
+                TrapCastChild();
+                break;
+            case "Throw_Dagger":
+                ThrowDagger();
+                break;
+            case "ChargeAttack":
+                ChargeAttack();
+                break;
+            case "Attack1":
+                Attack1();
+                isAttack1 = true;
+                break;
+            case "SpecialAttack":
+                SpecialAttack();
+                break;
+
+        }
+        yield return new WaitForSeconds(time);
+        isDelayAction = false;
+        weapon_hb.hasRepulsion = false;
+        weapon_hb.repulsion = 0;
+
+    }
+
+
+    IEnumerator Shoot()
+    {
+        yield return new WaitForSeconds(0.3f);
+        number_of_dagger--;
+        Instantiate(dagger_throw, throwPoint.position, throwPoint.rotation);
+
+    }
+    IEnumerator SkillDagger()
+    {
+        yield return new WaitForSeconds(0.5f);
+
+        Instantiate(skill_dagger, throwPoint.position, throwPoint.rotation);
+
+    }
+
+    void SpawnDustEffect(GameObject dust, float dustXOffset = 0)
+    {
+        if (dust != null)
+        {
+            // Set dust spawn position
+            Vector3 dustSpawnPosition = transform.position + new Vector3(dustXOffset * character_movement.m_facingDirection, 0.0f, 0.0f);
+            GameObject newDust = Instantiate(dust, dustSpawnPosition, Quaternion.identity) as GameObject;
+            // Turn dust in correct X direction
+            newDust.transform.localScale = newDust.transform.localScale.x * new Vector3(character_movement.m_facingDirection, 1, 1);
+        }
+
+    }
+    public void TrapCastChild()
+    {
+        number_of_dagger--;
+
+        if (character_movement.m_facingDirection == 1)
+        {
+            throwPoint.rotation = Quaternion.Euler(0, 0, 135);
+        }
+        else
+        {
+            throwPoint.rotation = Quaternion.Euler(0, 0, 45);
+        }
+        StartCoroutine(SkillDagger());
+    }
+
+    void AE_runStop()
+    {
+        m_audioManager.PlaySound("RunStop");
+    }
+
+    void AE_footstep()
+    {
+        m_audioManager.PlaySound("Footstep");
+    }
+
+    void AE_Jump()
+    {
+        m_audioManager.PlaySound("Jump");
+    }
+
+    void AE_Landing()
+    {
+        m_audioManager.PlaySound("Landing");
+
+    }
+}
